13b9408ec50c78e10dfe5d66052fc85faa22219a
Khalid, Al-Kooheji <noplace@gmail.com>
2011-11-05 10:33:16 +0300

    Scene 2 only

--- Code/Game/scene2.cpp
+++ Code/Game/scene2.cpp
@@ -1,45 +1,23 @@
 #include "scene2.h"
 
-struct SimpleVertex
-{
-    XMFLOAT2 Pos;
-    XMFLOAT2 Tex;
-    XMFLOAT4 Color;
-};
-
-
-
 int MainScene2::Initialize(aurora::Engine* engine) { 
   GameView::Initialize(engine);
 
   gui_.set_engine(engine);
   engine->process_manager().AddProcess(&gui_);
   shader_helper.Initialize(&engine->gfx_context());
-  
-  //aurora::resource::EffectResource* shader_res = engine_->resource_manager.GetResourceById<aurora::resource::EffectResource>(3);
-  {
 
+  {
     aurora::resource::Resource* shader_res = engine_->resource_manager.GetResourceById<aurora::resource::Resource>(3);
-    graphics::Effect::Entry entry_list[] = {
-      {"VS","vs_5_0"},
-      {"PS","ps_5_0"},
-      {"PSTex","ps_5_0"},
-      {"PSFont","ps_5_0"},
-      {NULL,NULL},
-    };
-    main_effect_.Initialize(&engine_->gfx_context());
-    main_effect_.CreateFromMemory2(entry_list,shader_res->data_pointer,shader_res->data_length);
     shader_helper.CreateFromMemory(shader_res->data_pointer,shader_res->data_length);
     shader_res->Unload();
   }
 
   {
     font_writer_.Initialize(&engine_->gfx_context());
-    aurora::resource::EffectResource* font_effect_ = engine_->resource_manager.GetResourceById<aurora::resource::EffectResource>(4);
     aurora::resource::FontResource* fr = engine_->resource_manager.GetResourceById<aurora::resource::FontResource>(7);
     font = fr->font();
     font_writer_.set_font(font);
-    font_writer_.set_effect(font_effect_->effect());
   }
 
   int hr;
@@ -52,29 +30,7 @@ int MainScene2::Initialize(aurora::Engine* engine) {
   gui_.AddElement(&mainmenu);
  
 
-  camera_.Ortho2D();
-  
-  // Create vertex buffer
-  SimpleVertex vertices[] =
-  {
-    { XMFLOAT2( 1.0f, -1.0f ), XMFLOAT2( 0.72070313f, 0.65820313f  ),XMFLOAT4(1.0f,1.0f,1.0f,1.0f) },  
-    { XMFLOAT2( 13.0f, -1.0f ), XMFLOAT2( 0.76757813f, 0.65820313f ),XMFLOAT4(1.0f,1.0f,1.0f,1.0f) },
-    { XMFLOAT2( 1.0f, 20.0f ), XMFLOAT2( 0.72070313f, 0.74023438f ),XMFLOAT4(1.0f,1.0f,1.0f,1.0f) },
-    { XMFLOAT2( 13.0f, 20.0f ), XMFLOAT2( 0.76757813f, 0.74023438f ),XMFLOAT4(1.0f,1.0f,1.0f,1.0f) },
-
-  };
-
-  SimpleVertex vertices2[] =
-  {
-    { camera_.OrthoPoint( 0.0f, 0.0f,0.0f, 0.0f ), XMFLOAT2( 0.0f, 0.0f ),XMFLOAT4(1.0f,0.0f,1.0f,1.0f) },  
-    { camera_.OrthoPoint( 1.0f, -40.0f,0.0f, 0.0f ), XMFLOAT2( 0.5f, 0.0f ),XMFLOAT4(1.0f,1.0f,0.0f,1.0f) },
-    { camera_.OrthoPoint( 0.0f, 0.0f,0.0f, 70.0f ), XMFLOAT2( 0.0f, 0.5f ),XMFLOAT4(0.0f,1.0f,1.0f,1.0f) },
-    { camera_.OrthoPoint( 1.0f, -40.0f,0.0f, 70.0f ), XMFLOAT2( 0.5f, 0.5f ),XMFLOAT4(1.0f,1.0f,1.0f,1.0f) },
-
-  };
-  
- 
-  
+    
   my_sprite.Initialize(&engine_->gfx_context());
   my_sprite.SetSize(20,25);
   my_sprite.SetTopLeft(300,100);
@@ -106,13 +62,6 @@ int MainScene2::Initialize(aurora::Engine* engine) {
   map.AddLayer();
   map.Construct();
 
-  g_pCBChangesEveryFrame.description.bind_flags = D3D11_BIND_CONSTANT_BUFFER;
-  g_pCBChangesEveryFrame.description.usage = D3D11_USAGE_DEFAULT;
-  g_pCBChangesEveryFrame.description.byte_width = sizeof(graphics::shader::ConstantBuffer2Type);
-  g_pCBChangesEveryFrame.description.cpu_access_flags = 0;
-  hr = engine_->gfx_context().CreateBuffer(g_pCBChangesEveryFrame,NULL);
-  if( FAILED( hr ) )
-      return hr;
 
   // Load the Texture
   //tex_res1 = (aurora::resource::TextureResource*)engine_->resource_manager.GetResourceById(5);
@@ -122,7 +71,7 @@ int MainScene2::Initialize(aurora::Engine* engine) {
   // Initialize the world matrices
   world = XMMatrixIdentity();
 
-  camera_.UpdateConstantBuffer();
+  
   motion1.set_context(&engine_->animation());
   motion1.set_delay_time(300);
   motion1.set_from(0);
@@ -155,14 +104,12 @@ int MainScene2::Deinitialize() {
 //  if( g_pTextureRV ) g_pTextureRV->Release();
   my_sprite.Deinitialize();
   if (engine_) {
-    engine_->gfx_context().DestroyBuffer(g_pCBChangesEveryFrame);
     engine_->gfx_context().DestroyBuffer(g_vb);
     engine_->gfx_context().DestroyBuffer(g_ib);
   }
 
   font_writer_.Deinitialize();
 
-  main_effect_.Deinitialize();
   shader_helper.Deinitialize();
   return S_OK;
 }
@@ -199,8 +146,6 @@ void MainScene2::Update(float delta_time) {
     font_writer_.WriteML(status1,strlen(status1),0);
   }
 
-    //camera_.view() = XMMatrixScaling(t,t,0);// * XMMatrixTranslation(0.5,0,0);
-  //camera_.UpdateConstantBuffer();
 
   const XMVECTOR vAxisZ = { 0, 0.0f, 2.0f, 0 };
   // Rotate cube around the origin
@@ -233,23 +178,12 @@ void MainScene2::Draw() {
   engine_->gfx_context().SetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP );
   */
 
-  
-  //main_effect_->Begin();
-  
+
   this->shader_helper.PrepareDraw();
   shader_helper.SetColorShader();
 
-  //engine_->gfx_context().SetInputLayout(main_effect_.input_layout());
-  //engine_->gfx_context().SetShader(main_effect_.vs(0));
-  //engine_->gfx_context().SetShader(main_effect_.ps(0));
-
-  //engine_->gfx_context().ClearShader(graphics::kShaderTypeGeometry);
-  //camera_.SetConstantBuffer(0);
-
   graphics::shader::ConstantBuffer2Type cb;
 
-  //engine_->gfx_context().SetConstantBuffers(graphics::kShaderTypeVertex,2,1,&g_pCBChangesEveryFrame);
-  //engine_->gfx_context().SetConstantBuffers(graphics::kShaderTypePixel,2,1,&g_pCBChangesEveryFrame);
 
   cb.world = XMMatrixTranspose( my_arc1.world() );
   cb.ps_color = my_arc1.color();
@@ -274,19 +208,15 @@ void MainScene2::Draw() {
   aurora::resource::TextureResource* sprite_texture = engine_->resource_manager.GetResourceById<aurora::resource::TextureResource>(100);
   ID3D11ShaderResourceView* srv = sprite_texture->srv();
   shader_helper.SetTextureShader();
-  //engine_->gfx_context().SetShader(main_effect_.ps(1));
   engine_->gfx_context().SetShaderResources(graphics::kShaderTypePixel,0,1,(void**)&(srv));
   cb.world = XMMatrixTranspose( my_sprite.world() );
   cb.ps_color = my_sprite.color();
   cb.enable_texture = true;
-  //engine_->gfx_context().UpdateSubresource(g_pCBChangesEveryFrame,&cb,NULL,0,0);
   shader_helper.UpdateChangesEveryFrame(&cb);
   my_sprite.Draw();
 
 
   shader_helper.SetTexturePagesShader();
-  //engine_->gfx_context().SetShader(main_effect_.ps(2));
-  //font_writer_.UpdateConstantBuffer();
   shader_helper.UpdateChangesEveryFrame(&font_writer_.misc_buffer_shader_);
   font_writer_.Draw();  
 
--- Code/Game/scene2.h
+++ Code/Game/scene2.h
@@ -14,17 +14,13 @@ class MainScene2 : public aurora::GameView {
   void Update(float delta_time);
   void Draw();
  private:
-  //graphics::Effect* main_effect_;
-   graphics::Effect main_effect_;
-  
+ 
   float move_z,move_x,move_y;
   aurora::animation::CubicMotion motion1;
   aurora::animation::Spiral spiral;
   graphics::font::Writer font_writer_;
   graphics::Buffer                    g_vb;
   graphics::Buffer                    g_ib;
-  graphics::Buffer                        g_pCBChangesEveryFrame;
-  //ID3D11ShaderResourceView*           g_pTextureRV ;
   XMMATRIX world;
   aurora::resource::TextureResource* tex_res1;
   acGraphics::Font* font;
